3dd8784c582f1a0dade38ff140756ffd151ff165
Rick South <rick@natamus.com>
2024-07-28 07:17:22 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/recipecommands/ModFabric.java
+++ Fabric/src/main/java/com/natamus/recipecommands/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.recipecommands;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.recipecommands.cmds.CommandRecipes;
 import com.natamus.recipecommands.util.Reference;
 import net.fabricmc.api.ModInitializer;
@@ -10,6 +11,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
